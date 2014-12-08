e0afb0a0932351a80639f4be9bd54705d657c32d
Bobby Kolev <bobbykolev@abv.bg>
2014-12-08 18:01:49 +0200

    FullHouse class

---
+++ src/hw_3/FullHouse.java
@@ -0,0 +1,14 @@
+package hw_3;
+
+/**
+ * Created by bkolev on 8.12.2014 г..
+ */
+public class FullHouse {
+    public static void main(String[] args) {
+        String[] cards = {"A", "K", "D", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
+        String[] colors = {"\u2660", "\u2665", "\u2666", "\u2663"};
+
+        int colorsLen = colors.length;
+        int cardsLen = cards.length;
+    }
+}
